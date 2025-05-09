f17994847247ff2691916d2f60b51c09b1ffd0a8
agussoul <aalma@mail.austral.edu.ar>
2025-05-09 14:16:57 -0300

    fixed tests and format

---
+++ test/app.e2e-module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '../src/prisma/prisma.module';
+import { UsersModule } from '../src/users/users.module';
+import { FilesModule } from '../src/files/files.module';
+import { StatsModule } from '../src/stats/stats.module';
+import { MAX_MONTHLY_BYTES_TEST } from '../src/utils/constants';
+
+// Sobrescribir MAX_MONTHLY_BYTES para pruebas
+import * as constants from '../src/utils/constants';
+(constants as any).MAX_MONTHLY_BYTES = MAX_MONTHLY_BYTES_TEST;
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    FilesModule,
+    StatsModule,
+  ],
+})
+export class AppE2eModule {}
